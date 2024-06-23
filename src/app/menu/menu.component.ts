d188bd5c59439183995250c8af51f5572761d1f2
MadAlexO <aluyo_2012@hotmail.com>
2024-06-22 21:07:26 -0500

    Retoques en el menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../angular-material/material/material.module';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
 
